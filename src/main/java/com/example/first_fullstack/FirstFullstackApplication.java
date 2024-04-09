85c9b3afcabbef231abbbf1e44ff9aaa73b9784b
Time_ON <haas.timeon@gmail.com>
2024-04-09 13:12:37 +0200

    Initial commit

---
+++ src/main/java/com/example/first_fullstack/FirstFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.example.first_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstFullstackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/first_fullstack/FirstFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.first_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstFullstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
